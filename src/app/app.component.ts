cd34a44eea54caf05cc5efb714eebe6fd4222a3e
unknown <amar.aj.github@gmail.com>
2022-10-10 17:20:39 +0530

    first com

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Cat } from './cat.mode';
+import { CatService } from './cat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-infinite-scroll-example';
+  
+  page = 1;
+  cats: Cat[] = []
+
+  constructor(private catService: CatService) {}
+
+  ngOnInit(): void {
+    this.catService
+      .getCats(this.page)
+      .subscribe((cats: Cat[]) => {
+        this.cats = cats;
+      });
+  }
+
+  onScroll(): void {
+    this.catService
+      .getCats(++this.page)
+      .subscribe((cats: Cat[]) => {
+        this.cats.push(...cats);
+      });
+  }
+
+  doStuff(name:string)
+  {
+    console.log(name);
+  }
+}
